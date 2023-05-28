2c027b9773aea208d36f3909d757ea28b45d8a82
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-05-28 19:15:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("19911978"))
+API_ID = int(getenv('19911978'))
 API_HASH = getenv("e3f5848d4c384af9e6f1f52ca84c19c7")
 
 BOT_TOKEN = getenv("6028079887:AAGoPNp6LhbpXkcExuXPVTQeoo24p25rO74")
-OWNER_ID = int(getenv("6205468574"))
+OWNER_ID = int(getenv('6205468574'))
 
 MONGO_DB_URI = getenv("mongodb+srv://NischayP:NischayP@cluster0.9cuf6ae.mongodb.net/?retryWrites=true&w=majority")
